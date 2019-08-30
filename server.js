10cadf3a6f78c2114165022fd0a4bac9df25a999
lendoo73 <48564722+lendoo73@users.noreply.github.com>
2019-08-30 18:38:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+// link the app... .js
+app.use(require('./appPost.js'));
+
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
-
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
-});
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
